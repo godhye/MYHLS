00cbed2f1ec13478842ebe0e3efb43763f6d3328
godhye <exmr12@gmail.com>
2021-10-09 12:59:59 +0900

    hanler func implement

--- main.go
+++ main.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"net/http"
-	"net/url"
 )
 
+type testHandler struct {
+	http.Handler
+}
+
+func (h *testHandler) ServeHTTP(W http.ResponseWriter, req *http.Request) {
+	str := "Request path is " + req.URL.Path + "\n"
+	str += "Parameter    is " + req.URL.RawQuery + "\n"
+	W.Write([]byte(str))
+}
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-
-	http.HandleFunc("/bye", func(w http.ResponseWriter, req *http.Request) {
-
-		//파라미터 출력을 위한 파라미터 파싱
-		parameter, err := url.ParseQuery(req.URL.RawQuery)
-		if err == nil {
-			for k, v := range parameter {
-				w.Write([]byte(k + " "))
-				for i := 0; i < len(v); i++ {
-					w.Write([]byte(v[i]))
-				}
-
-				w.Write([]byte("\n"))
-
-			}
-		}
-
-		w.Write([]byte("bye world  with "))
-	})
+	http.Handle("/bye", new(testHandler))
 
 	http.ListenAndServe(":5000", nil)
 }
