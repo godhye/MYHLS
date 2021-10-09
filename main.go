a9ea1054979fdb1984b79a7058a5f5576ed285bd
godhye <exmr12@gmail.com>
2021-10-09 11:53:15 +0900

    parameter parsing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -11,7 +12,22 @@ func main() {
 	})
 
 	http.HandleFunc("/bye", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("bye world  with ," + req.URL.RawQuery))
+
+		//파라미터 출력을 위한 파라미터 파싱
+		parameter, err := url.ParseQuery(req.URL.RawQuery)
+		if err == nil {
+			for k, v := range parameter {
+				w.Write([]byte(k + " "))
+				for i := 0; i < len(v); i++ {
+					w.Write([]byte(v[i]))
+				}
+
+				w.Write([]byte("\n"))
+
+			}
+		}
+
+		w.Write([]byte("bye world  with "))
 	})
 
 	http.ListenAndServe(":5000", nil)
