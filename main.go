a8caa54c9eca50a1c86a0a22291adf3d914898b1
godhye <exmr12@gmail.com>
2021-10-24 21:16:00 +0900

    http server abs

--- main.go
+++ main.go
@@ -1,67 +1,30 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"net/http"
-	"path/filepath"
 )
-
-type staticHandler struct {
-	http.Handler
-}
-
-func getContentType(localpath string) string {
-	contenttype := ""
-	ext := filepath.Ext(localpath)
-
-	switch ext {
-
-	case ".html":
-		contenttype = "text.html"
-	case ".css":
-		contenttype = "text.css"
-	case ".js":
-		contenttype = "text.js"
-	case ".png":
-		contenttype = "text.png"
-	}
-
-	return contenttype
-}
-func (h *staticHandler) ServeHTTP(W http.ResponseWriter, req *http.Request) {
-	//파일 위치 읽기
-	localPath := "data" + req.URL.Path
-	content, err := ioutil.ReadFile(localPath)
-	if err != nil {
-		W.WriteHeader(404)
-		W.Write([]byte(http.StatusText(404)))
-		return
-	}
-
-	contentType := getContentType(localPath)
-
-	//파일 형식 지정
-	W.Header().Add("Content-Type", contentType)
-	W.Write(content)
-	//응답
-}
-
-type testHandler struct {
-	http.Handler
-}
-
-//구조체 testHandler의 메소드 구현
-func (h *testHandler) ServeHTTP(W http.ResponseWriter, req *http.Request) {
-	//Write([]byte)를 이용해 바로 쓰는거 보다 깔끔하게 string에 담아서 쓰기
-	str := "Request path is " + req.URL.Path + "\n"
-	str += "Parameter    is " + req.URL.RawQuery + "\n"
-	W.Write([]byte(str))
-}
 
 func main() {
 
-	http.Handle("/bye", new(testHandler))
-	http.Handle("/", new(staticHandler))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf(req.URL.Path + "\n")
+		p := "." + req.URL.Path
+		if p == "./" {
+			p = "./data/home.html"
+		}
+		http.ServeFile(w, req, p)
+	})
 
+	http.Handle("/data", http.FileServer(http.Dir(".")))
+
+	http.HandleFunc("/hls", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf(req.URL.Path)
+		p := "." + req.URL.Path
+		if p == "./hls" {
+			p = "./data/test.html"
+		}
+		http.ServeFile(w, req, p)
+	})
 	http.ListenAndServe(":5000", nil)
 }
