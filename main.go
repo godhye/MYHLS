b691820c327f6eee6b73aa9aa2bc37326f5162f2
godhye <exmr12@gmail.com>
2021-10-31 07:02:50 +0900

    주석추가

--- main.go
+++ main.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-
+	//127.0.0.1:5000/
+	//메인 홈페이지 , homehtml 에는 사진 , 사진위에 글씨 출력
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Printf(req.URL.Path + "\n")
 		p := "." + req.URL.Path
 		if p == "./" {
 			p = "./data/home.html"
 		}
+		//p에 있는 파일을 w에 담아서 보냄
 		http.ServeFile(w, req, p)
 	})
 
+	//127.0.0.1:5000/data ./data폴더에 있는 파일 다운로드 서버 제공
 	http.Handle("/data", http.FileServer(http.Dir(".")))
 
+	//127.0.0.1:5000/hls  버튼 눌러 동영상 재생 , hls 제공 예정
 	http.HandleFunc("/hls", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Printf(req.URL.Path)
 		p := "." + req.URL.Path
